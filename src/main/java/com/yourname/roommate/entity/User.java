e37803e2bcfbf3e93e82a6a92e66f4230250b3a8
Ayushmangiri <ayushmangiri@gmail.com>
2025-09-21 19:10:45 +0530

    Initial commit

---
+++ src/main/java/com/yourname/roommate/entity/User.java
@@ -0,0 +1,30 @@
+package com.yourname.roommate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.management.relation.Role;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String phone;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum  Role {
+        USER,
+        ADMIN
+    }
+}
---
+++ src/test/java/com/yourname/roommate/RoommateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourname.roommate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoommateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
